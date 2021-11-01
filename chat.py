b0c98f3905e33b5e38258aeec5490fad4157511b
Rofita Siti Musdalifah <rofitasitimusdalifah@gmail.com>
2021-11-01 21:26:32 +0700

    add chat socket

---
+++ chat.py
@@ -0,0 +1,121 @@
+import sys
+import os
+import json
+import uuid
+import logging
+from queue import  Queue
+
+class Chat:
+	def __init__(self):
+		self.sessions={}
+		self.users = {}
+		self.users['pevita']={ 'nama': 'Pevita Pierce', 'negara': 'Amerika', 'password': 'kijits', 'incoming' : {}, 'outgoing': {}}
+		self.users['maudy']={ 'nama': 'Maudy Ayunda', 'negara': 'Inggris', 'password': 'kijits', 'incoming': {}, 'outgoing': {}}
+		self.users['nadiem']={ 'nama': 'Nadiem Makarim', 'negara': 'Indonesia', 'password': 'kijits','incoming': {}, 'outgoing':{}}
+	def proses(self,data):
+		j=data.split(" ")
+		try:
+			command=j[0].strip()
+			if (command=='auth'):
+				username=j[1].strip()
+				password=j[2].strip()
+				logging.warning("AUTH: auth {} {}" . format(username,password))
+				return self.autentikasi_user(username,password)
+			elif (command=='send'):
+				sessionid = j[1].strip()
+				usernameto = j[2].strip()
+				message=""
+				for w in j[3:]:
+					message="{} {}" . format(message,w)
+				usernamefrom = self.sessions[sessionid]['username']
+				logging.warning("SEND: session {} send message from {} to {}" . format(sessionid, usernamefrom,usernameto))
+				return self.send_message(sessionid,usernamefrom,usernameto,message)
+			elif (command=='inbox'):
+				sessionid = j[1].strip()
+				username = self.sessions[sessionid]['username']
+				logging.warning("INBOX: {}" . format(sessionid))
+				return self.get_inbox(username)
+			else:
+				return {'status': 'ERROR', 'message': '**Protocol Tidak Benar'}
+		except KeyError:
+			return { 'status': 'ERROR', 'message' : 'Informasi tidak ditemukan'}
+		except IndexError:
+			return {'status': 'ERROR', 'message': '--Protocol Tidak Benar'}
+	def autentikasi_user(self,username,password):
+		if (username not in self.users):
+			return { 'status': 'ERROR', 'message': 'User Tidak Ada' }
+		if (self.users[username]['password']!= password):
+			return { 'status': 'ERROR', 'message': 'Password Salah' }
+		tokenid = str(uuid.uuid4()) 
+		self.sessions[tokenid]={ 'username': username, 'userdetail':self.users[username]}
+		return { 'status': 'OK', 'tokenid': tokenid }
+	def get_user(self,username):
+		if (username not in self.users):
+			return False
+		return self.users[username]
+	def send_message(self,sessionid,username_from,username_dest,message):
+		if (sessionid not in self.sessions):
+			return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+		s_fr = self.get_user(username_from)
+		s_to = self.get_user(username_dest)
+		
+		if (s_fr==False or s_to==False):
+			return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+
+		message = { 'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message }
+		outqueue_sender = s_fr['outgoing']
+		inqueue_receiver = s_to['incoming']
+		try:	
+			outqueue_sender[username_from].put(message)
+		except KeyError:
+			outqueue_sender[username_from]=Queue()
+			outqueue_sender[username_from].put(message)
+		try:
+			inqueue_receiver[username_from].put(message)
+		except KeyError:
+			inqueue_receiver[username_from]=Queue()
+			inqueue_receiver[username_from].put(message)
+		return {'status': 'OK', 'message': 'Message Sent'}
+
+	def get_inbox(self,username):
+		s_fr = self.get_user(username)
+		incoming = s_fr['incoming']
+		msgs={}
+		for users in incoming:
+			msgs[users]=[]
+			while not incoming[users].empty():
+				msgs[users].append(s_fr['incoming'][users].get_nowait())
+			
+		return {'status': 'OK', 'messages': msgs}
+
+
+if __name__=="__main__":
+	j = Chat()
+	sesi = j.proses("auth maudy kijits")
+	print(sesi)
+	#sesi = j.autentikasi_user('maudy','kijits')
+	#print sesi
+	tokenid = sesi['tokenid']
+	print(j.proses("send {} nadiem hello nadiem" . format(tokenid)))
+	print(j.proses("send {} pevita hello apa kabar pev" . format(tokenid)))
+
+	print("isi mailbox dari pevita")
+	print(j.get_inbox('pevita'))
+	print("isi mailbox dari nadiem")
+	print(j.get_inbox('nadiem'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
